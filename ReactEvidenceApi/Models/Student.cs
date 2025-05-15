a76ea02bc4f91c325fc50f465d74e49e7b3b0135
Mst Nusrat Jahan Jisa <njjisa499@gmail.com>
2025-05-15 18:38:32 +0600

    Add project files.

---
+++ ReactEvidenceApi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace ReactEvidenceApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime AdmissionDate { get; set; } = DateTime.Now;
+        public int CourseFee {  get; set; }
+        public bool IsActive { get; set; }
+
+        public string? ImageUrl { get; set; }
+        public List<Address> Addresses { get; set; } = new List<Address>();
+    }
+}
